d1458b8be5b965e942ccb86165bb017cd08ff9b2
Pinkpapa-69 <jycho69@naver.com>
2022-03-18 01:41:09 -0400

    app.py modify

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
  
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World! 2nd...'
  
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
